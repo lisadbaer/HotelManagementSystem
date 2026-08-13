0526cb9e36204af77e02071f6b4399f680fed0d4
Lisa Baer <lisa.baer@ashesi.edu.gh>
2026-08-13 17:19:15 +0000

    New files more Edits

--- database.py
+++ database.py
@@ -1,11 +1,25 @@
 import mariadb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_connection():
+
     return mariadb.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password=" ",
-        database="hostel_management",
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD="),
+        database=os.getenv("Hostel_Management"),
     )
+
+
+# def get_connection():
+#   return mariadb.connect(
+#      host="localhost",
+#      port=3306,
+#      user="root",
+#        password=" ",
+#       database="hostel_management")
